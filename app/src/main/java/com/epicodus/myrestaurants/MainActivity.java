a05b79ad6453b89b14377ceb7a19aedb73279f3d
Courtney Phillips <courtney@epicodus.com>
2016-04-18 15:42:46 -0700

    Add Extra to attach location data to Intent in MainActivity.

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -25,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String location = mLocationEditText.getText().toString();
-                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                Intent.putExtra("location", location);
                 startActivity(intent);
             }
         });
