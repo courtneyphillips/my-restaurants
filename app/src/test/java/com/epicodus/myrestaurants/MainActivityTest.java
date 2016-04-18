a8dc01739ba1b278cf1ad0078806a0aba2bdd17a
Courtney Phillips <courtney@epicodus.com>
2016-04-18 16:15:08 -0700

    Fix typo in build.gradle file.

--- app/src/test/java/com/epicodus/myrestaurants/MainActivityTest.java
+++ app/src/test/java/com/epicodus/myrestaurants/MainActivityTest.java
@@ -1,6 +1,14 @@
 package com.epicodus.myrestaurants;
 
+import org.junit.Before;
+
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
 @RunWith(RoboelectricGradleTestRunner.class)
 public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Roboelectric.setupActivity(MainActivity.class);
+    }
 }
