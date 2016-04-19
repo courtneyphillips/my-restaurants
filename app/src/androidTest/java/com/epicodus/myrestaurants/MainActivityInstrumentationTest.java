c3ca734e99c4ecbc1a75a705a4699b5352e0e063
Courtney Phillips <courtney@epicodus.com>
2016-04-19 11:12:50 -0700

    Add rule to specify which activity following tests require to launch first.

--- app/src/androidTest/java/com/epicodus/myrestaurants/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/epicodus/myrestaurants/MainActivityInstrumentationTest.java
@@ -1,7 +1,14 @@
 package com.epicodus.myrestaurants;
 
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+
 /**
  * Created by staff on 4/19/16.
  */
 public class MainActivityInstrumentationTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
 }
