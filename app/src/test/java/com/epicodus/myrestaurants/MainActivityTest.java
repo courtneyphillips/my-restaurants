8f663c76276f2aa4f1be8deb94a0dc4a993d044b
Courtney Phillips <courtney@epicodus.com>
2016-04-19 10:44:01 -0700

    Add test to assert text in TextView is 'MyRestaurants', as expected. Test passes.

--- app/src/test/java/com/epicodus/myrestaurants/MainActivityTest.java
+++ app/src/test/java/com/epicodus/myrestaurants/MainActivityTest.java
@@ -1,14 +1,30 @@
 package com.epicodus.myrestaurants;
 
+import android.os.Build;
+import android.widget.TextView;
+
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertTrue;
 
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
-@RunWith(RoboelectricGradleTestRunner.class)
+@RunWith(RobolectricGradleTestRunner.class)
 public class MainActivityTest {
     private MainActivity activity;
 
     @Before
     public void setup() {
-        activity = Roboelectric.setupActivity(MainActivity.class);
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView appNameTextView = (TextView) activity.findViewById(R.id.appNameTextView);
+        assertTrue("MyRestaurants".equals(appNameTextView.getText().toString()));
     }
 }
