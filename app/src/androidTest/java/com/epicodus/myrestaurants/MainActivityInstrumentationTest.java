51832daba9efee37aef6730a40e3059cbe959709
Courtney Phillips <courtney@epicodus.com>
2016-04-19 11:18:24 -0700

    Add test to verify text entered in locationEditText is 'Portland'.

--- app/src/androidTest/java/com/epicodus/myrestaurants/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/epicodus/myrestaurants/MainActivityInstrumentationTest.java
@@ -3,12 +3,22 @@ package com.epicodus.myrestaurants;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
+import org.junit.Test;
 
-/**
- * Created by staff on 4/19/16.
- */
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 public class MainActivityInstrumentationTest {
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule =
             new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.locationEditText)).perform(typeText("Portland"))
+                .check(matches(withText("Portland")));
+    }
 }
