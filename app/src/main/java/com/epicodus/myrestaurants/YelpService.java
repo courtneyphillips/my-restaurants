f9c196da594e97e86f36083a5a65e881239e51ba
Courtney Phillips <courtney@epicodus.com>
2016-04-20 10:44:21 -0700

    Add code to make authenticated request to Yelp API.

--- app/src/main/java/com/epicodus/myrestaurants/YelpService.java
+++ app/src/main/java/com/epicodus/myrestaurants/YelpService.java
@@ -1,7 +1,39 @@
 package com.epicodus.myrestaurants;
 
+import javax.security.auth.callback.Callback;
+
+import okhttp3.Call;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
+import se.akerfeldt.okhttp.signpost.SigningInterceptor;
+
 /**
  * Created by staff on 4/20/16.
  */
 public class YelpService {
+    public static void findRestaurants(String location, Callback callback) {
+        String CONSUMER_KEY = Constants.YELP_CONSUMER_KEY;
+        String CONSUMER_SECRET = Constants.YELP_CONSUMER_SECRET;
+        String TOKEN = Constants.YELP_TOKEN;
+        String TOKEN_SECRET = Constants.YELP_TOKEN_SECRET;
+        OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
+        consumer.setTokenWithSecret(TOKEN, TOKEN_SECRET);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new SigningInterceptor(consumer))
+                .build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, location);
+        String url = urlBuilder.build().toString();
+
+        Request request= new Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
 }
