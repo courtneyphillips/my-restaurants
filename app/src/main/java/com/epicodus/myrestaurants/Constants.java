ad2781a5b2ec44b0347e2be9f725d8425b991ec8
Courtney Phillips <courtney@epicodus.com>
2016-04-20 10:33:32 -0700

    Add references to Yelp credentials in gradle.properties into new Constants.java class.

--- app/src/main/java/com/epicodus/myrestaurants/Constants.java
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -4,4 +4,8 @@ package com.epicodus.myrestaurants;
  * Created by staff on 4/20/16.
  */
 public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
 }
