433292f3a50dbe25431d16a66bacd25e3a05f2d1
Courtney Phillips <courtney@epicodus.com>
2016-04-25 14:30:03 -0700

    Add click listener in RecyclerView to take users to restaurant detail view.

--- app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantListAdapter.java
@@ -2,6 +2,7 @@
 package com.epicodus.myrestaurants.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +12,10 @@ import android.widget.TextView;
 
 import com.epicodus.myrestaurants.R;
 import com.epicodus.myrestaurants.models.Restaurant;
+import com.epicodus.myrestaurants.ui.RestaurantDetailActivity;
 import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
@@ -45,19 +49,26 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
     }
 
     public class RestaurantViewHolder extends RecyclerView.ViewHolder {
-        @Bind(R.id.restaurantImageView)
-        ImageView mRestaurantImageView;
-        @Bind(R.id.restaurantNameTextView)
-        TextView mNameTextView;
+        @Bind(R.id.restaurantImageView) ImageView mRestaurantImageView;
+        @Bind(R.id.restaurantNameTextView) TextView mNameTextView;
         @Bind(R.id.categoryTextView) TextView mCategoryTextView;
         @Bind(R.id.ratingTextView) TextView mRatingTextView;
         private Context mContext;
-
 
         public RestaurantViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
             mContext = itemView.getContext();
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int itemPosition = getLayoutPosition();
+                    Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
+                    intent.putExtra("position", itemPosition + "");
+                    intent.putExtra("restaurants", Parcels.wrap(mRestaurants));
+                    mContext.startActivity(intent);
+                }
+            });
         }
 
         public void bindRestaurant(Restaurant restaurant) {
