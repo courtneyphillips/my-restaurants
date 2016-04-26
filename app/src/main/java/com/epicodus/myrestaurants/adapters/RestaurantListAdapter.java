248f0c91b39282870a5bb106508188cc12baa417
Courtney Phillips <courtney@epicodus.com>
2016-04-26 11:29:44 -0700

    Resize images with Picasso in RestaurantListAdapter to prevent OutOfMemory errors.

--- app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantListAdapter.java
@@ -23,13 +23,13 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder> {
+    private static final int MAX_WIDTH = 200;
+    private static final int MAX_HEIGHT = 200;
+
     private ArrayList<Restaurant> mRestaurants = new ArrayList<>();
     private Context mContext;
 
     public RestaurantListAdapter(Context context, ArrayList<Restaurant> restaurants) {
-        private static final int MAX_WIDTH = 200;
-        private static final int MAX_HEIGHT = 200;
-
         mContext = context;
         mRestaurants = restaurants;
     }
@@ -76,6 +76,13 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
 
         public void bindRestaurant(Restaurant restaurant) {
             Picasso.with(mContext).load(restaurant.getImageUrl()).into(mRestaurantImageView);
+
+            Picasso.with(mContext)
+                    .load(restaurant.getImageUrl())
+                    .resize(MAX_WIDTH, MAX_HEIGHT)
+                    .centerCrop()
+                    .into(mRestaurantImageView);
+
             mNameTextView.setText(restaurant.getName());
             mCategoryTextView.setText(restaurant.getCategories().get(0));
             mRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
