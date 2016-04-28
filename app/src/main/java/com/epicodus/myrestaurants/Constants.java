4d2a82ee9a7a0e6fe1deaf9d8f08affd58bc4e04
Courtney Phillips <courtney@epicodus.com>
2016-04-28 11:06:33 -0700

    Add firebase root URL to project.

--- app/src/main/java/com/epicodus/myrestaurants/Constants.java
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -1,15 +1,13 @@
 package com.epicodus.myrestaurants;
 
-/**
- * Created by staff on 4/20/16.
- */
 public class Constants {
     public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
     public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
     public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
     public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
-
     public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?term=food";
     public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
     public static final String PREFERENCES_LOCATION_KEY = "location";
+    public static final String FIREBASE_URL = BuildConfig.FIREBASE_ROOT_URL;
+
 }
