8c0e1851686b6a5878ccd0c2edb9b2b6bed4a0ba
Courtney Phillips <courtney@epicodus.com>
2016-04-29 10:02:17 -0700

    Add shared preferences code back in, but leave commented out, so that we may resume saving zip codes to shared preferences later on.

--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -40,12 +40,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (v == mFindRestaurantsButton) {
             String location = mLocationEditText.getText().toString();
+            // if(!(location).equals("")) {
+            //       addToSharedPreferences(location);
+            //   }
             saveLocationToFirebase(location);
             Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
             intent.putExtra("location", location);
             startActivity(intent);
         }
     }
+
+    // private void addToSharedPreferences(String location) {
+    //    mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
+    //  }
 
     public void saveLocationToFirebase(String location) {
         Firebase searchedLocationRef = new Firebase(Constants.FIREBASE_URL_SEARCHED_LOCATION);
