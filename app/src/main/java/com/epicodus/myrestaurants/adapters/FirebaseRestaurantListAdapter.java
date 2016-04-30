8603a96be6d2c461c05bf096825b7434dcfcc5b9
Courtney Phillips <courtney@epicodus.com>
2016-04-29 17:15:06 -0700

    Add constructor to FirebaseRestaurantListAdapter.

--- app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantListAdapter.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantListAdapter.java
@@ -2,6 +2,10 @@ package com.epicodus.myrestaurants.adapters;
 
 import com.epicodus.myrestaurants.models.Restaurant;
 import com.epicodus.myrestaurants.util.FirebaseRecyclerAdapter;
+import com.firebase.client.Query;
 
 public class FirebaseRestaurantListAdapter extends FirebaseRecyclerAdapter<RestaurantViewHolder, Restaurant> {
+    public FirebaseRestaurantListAdapter(Query query, Class<Restaurant> itemClass) {
+        super(query, itemClass);
+    }
 }
