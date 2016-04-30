0d24c4db8f534a326f54336fcae5096ecae4c536
Courtney Phillips <courtney@epicodus.com>
2016-04-29 17:12:49 -0700

    Extend FirebaseRecyclerAdapter in new FirebaseRestaurantListAdapter class.

--- app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantListAdapter.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantListAdapter.java
@@ -1,7 +1,7 @@
 package com.epicodus.myrestaurants.adapters;
 
-/**
- * Created by staff on 4/29/16.
- */
-public class FirebaseRestaurantListAdapter {
+import com.epicodus.myrestaurants.models.Restaurant;
+import com.epicodus.myrestaurants.util.FirebaseRecyclerAdapter;
+
+public class FirebaseRestaurantListAdapter extends FirebaseRecyclerAdapter<RestaurantViewHolder, Restaurant> {
 }
