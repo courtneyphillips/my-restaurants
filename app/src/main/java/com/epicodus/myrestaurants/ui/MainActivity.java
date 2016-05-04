afaabe2e527178b6cdced974f4d8db8b450cd25a
Courtney Phillips <courtney@epicodus.com>
2016-05-03 22:49:33 -0700

    Call
    takeUserToLoginScreenOnUnAuth method in new logout method.

--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     protected void logout() {
         mFirebaseRef.unauth();
+        takeUserToLoginScreenOnUnAuth();
     }
 
     private void takeUserToLoginScreenOnUnAuth() {
