f94531962384b6584b4bcd2ab57a0b7c3c1aee48
Courtney Phillips <courtney@epicodus.com>
2016-05-03 22:47:48 -0700

    Add conditional to existing onOptionsItemSelected method to check if logout option was selected.

--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -51,6 +51,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
