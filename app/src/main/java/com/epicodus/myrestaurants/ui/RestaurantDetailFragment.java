1cc12c7083a1b5a703a78d25d5140f67d69c99fb
Courtney Phillips <courtney@epicodus.com>
2016-05-05 14:33:08 -0700

    Fix bug in RestaurantDetailFragment causing inconsistencies in restaurant push IDs. Thanks Abby! :)

--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantDetailFragment.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantDetailFragment.java
@@ -107,7 +107,7 @@ public class RestaurantDetailFragment extends Fragment implements View.OnClickLi
             Firebase pushRef = userRestaurantsFirebaseRef.push();
             String restaurantPushId = pushRef.getKey();
             mRestaurant.setPushId(restaurantPushId);
-            userRestaurantsFirebaseRef.push().setValue(mRestaurant);
+            userRestaurantsFirebaseRef.setValue(mRestaurant);
             Toast.makeText(getContext(), "Saved", Toast.LENGTH_SHORT).show();
         }
     }
