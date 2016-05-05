9fe1eab21a4052bb37140c54bfe85546af0718a9
Courtney Phillips <courtney@epicodus.com>
2016-05-05 16:16:17 -0700

    Declare and define mAdapter as the ItemTouchHelperAdapter for use in SimpleItemTouchHelperCallback.java.

--- app/src/main/java/com/epicodus/myrestaurants/util/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/com/epicodus/myrestaurants/util/SimpleItemTouchHelperCallback.java
@@ -3,4 +3,10 @@ package com.epicodus.myrestaurants.util;
 import android.support.v7.widget.helper.ItemTouchHelper;
 
 public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
+    private final ItemTouchHelperAdapter mAdapter;
+
+    public SimpleItemTouchHelperCallback(ItemTouchHelperAdapter adapter) {
+        mAdapter = adapter;
+    }
+
 }
