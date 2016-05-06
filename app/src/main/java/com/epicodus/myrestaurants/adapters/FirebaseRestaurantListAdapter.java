99d8382f4c22b7225ec4efd21df8539a83ee8b67
Courtney Phillips <courtney@epicodus.com>
2016-05-06 11:36:04 -0700

    Implement ItemTouchHelperAdapter in FirebaseRestaurantListAdapter.

--- app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantListAdapter.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantListAdapter.java
@@ -1,29 +1,76 @@
 package com.epicodus.myrestaurants.adapters;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v4.view.MotionEventCompat;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
 import com.epicodus.myrestaurants.models.Restaurant;
 import com.epicodus.myrestaurants.util.FirebaseRecyclerAdapter;
+import com.epicodus.myrestaurants.util.ItemTouchHelperAdapter;
+import com.epicodus.myrestaurants.util.OnStartDragListener;
+import com.firebase.client.Firebase;
 import com.firebase.client.Query;
 
-public class FirebaseRestaurantListAdapter extends FirebaseRecyclerAdapter<RestaurantViewHolder, Restaurant> {
-    public FirebaseRestaurantListAdapter(Query query, Class<Restaurant> itemClass) {
+import java.util.Collections;
+
+public class FirebaseRestaurantListAdapter extends FirebaseRecyclerAdapter<RestaurantViewHolder, Restaurant> implements ItemTouchHelperAdapter {
+    private final OnStartDragListener mDragStartListener;
+    private Context mContext;
+
+    public FirebaseRestaurantListAdapter(Query query, Class<Restaurant> itemClass, OnStartDragListener dragStartListener) {
         super(query, itemClass);
+        mDragStartListener = dragStartListener;
     }
 
     @Override
     public RestaurantViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mContext = parent.getContext();
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.restaurant_list_item, parent, false);
+                .inflate(R.layout.restaurant_list_item_drag, parent, false);
         return new RestaurantViewHolder(view, getItems());
     }
 
     @Override
     public void onBindViewHolder(RestaurantViewHolder holder, int position) {
         holder.bindRestaurant(getItem(position));
+        holder.mRestaurantImageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (MotionEventCompat.getActionMasked(event) == MotionEvent.ACTION_DOWN) {
+                    mDragStartListener.onStartDrag(holder);
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onItemMove(int fromPosition, int toPosition) {
+        Collections.swap(getItems(), fromPosition, toPosition);
+        notifyItemMoved(fromPosition, toPosition);
+        return true;
+    }
+
+    @Override
+    public void onItemDismiss(int position) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+        String uid = sharedPreferences.getString(Constants.KEY_UID, null);
+        Firebase ref = new Firebase(Constants.FIREBASE_URL_RESTAURANTS).child(uid);
+        String restaurantKey = getItem(position).getPushId();
+        ref.child(restaurantKey).removeValue();
+    }
+
+    @Override
+    public int getItemCount() {
+        return getItems().size();
     }
 
     @Override
