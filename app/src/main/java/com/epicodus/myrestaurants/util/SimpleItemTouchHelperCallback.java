a8ec1ee46c2f733acf033335a8983468ab85a3cb
Courtney Phillips <courtney@epicodus.com>
2016-05-06 11:15:47 -0700

    Add getMovementFlags method to enable supported motions in drag and swipe features.

--- app/src/main/java/com/epicodus/myrestaurants/util/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/com/epicodus/myrestaurants/util/SimpleItemTouchHelperCallback.java
@@ -1,5 +1,6 @@
 package com.epicodus.myrestaurants.util;
 
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 
 public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
@@ -18,5 +19,13 @@ public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
     public boolean isItemViewSwipeEnabled() {
         return true;
     }
+
+    @Override
+    public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        final int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+        final int swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END;
+        return makeMovementFlags(dragFlags, swipeFlags);
+    }
+
 
 }
