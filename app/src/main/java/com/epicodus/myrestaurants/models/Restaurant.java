f4628e4c1445455d790a3e303def5f48e40c2bb4
Courtney Phillips <courtney@epicodus.com>
2016-05-06 11:49:13 -0700

    Add index attribute to Restaurant model to begin saving order of restaurants in Firebase.

--- app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
+++ app/src/main/java/com/epicodus/myrestaurants/models/Restaurant.java
@@ -16,6 +16,7 @@ public class Restaurant {
     double longitude;
     List<String> categories = new ArrayList<>();
     private String pushId;
+    private String index;
 
     public Restaurant() {}
 
@@ -31,6 +32,7 @@ public class Restaurant {
         this.latitude = latitude;
         this.longitude = longitude;
         this.categories = categories;
+        this.index = "not_specified";
     }
 
     public String getName() {
@@ -78,5 +80,13 @@ public class Restaurant {
 
     public void setPushId(String pushId) {
         this.pushId = pushId;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
     }
 }
