27713c95efc15f43940bbf582caff145130fa40d
Courtney Phillips <courtney@epicodus.com>
2016-05-06 11:39:44 -0700

    Add ItemTouchHelper to SavedRestaurantListActivity; implement OnStartDragListener.

--- app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListActivity.java
@@ -6,31 +6,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
 import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
 import com.epicodus.myrestaurants.adapters.FirebaseRestaurantListAdapter;
 import com.epicodus.myrestaurants.models.Restaurant;
+import com.epicodus.myrestaurants.util.OnStartDragListener;
+import com.epicodus.myrestaurants.util.SimpleItemTouchHelperCallback;
 import com.firebase.client.Firebase;
 import com.firebase.client.Query;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class SavedRestaurantListActivity extends AppCompatActivity {
+public class SavedRestaurantListActivity extends AppCompatActivity implements OnStartDragListener {
     private Query mQuery;
     private Firebase mFirebaseRestaurantsRef;
     private FirebaseRestaurantListAdapter mAdapter;
     private SharedPreferences mSharedPreferences;
-
-    @Bind(R.id.recyclerView)
-    RecyclerView mRecyclerView;
+    private ItemTouchHelper mItemTouchHelper;
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
         ButterKnife.bind(this);
+
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         mFirebaseRestaurantsRef = new Firebase(Constants.FIREBASE_URL_RESTAURANTS);
 
@@ -45,8 +48,16 @@ public class SavedRestaurantListActivity extends AppCompatActivity {
     }
 
     private void setUpRecyclerView() {
-        mAdapter = new FirebaseRestaurantListAdapter(mQuery, Restaurant.class);
+        mAdapter = new FirebaseRestaurantListAdapter(mQuery, Restaurant.class, this);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(mAdapter);
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
     }
 }
