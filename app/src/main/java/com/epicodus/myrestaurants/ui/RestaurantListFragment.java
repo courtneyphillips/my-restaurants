f62ccd70e392fee8a1ceb606ac83f00f4e740a85
courtney <courtney.mae.phillips@gmail.com>
2016-05-09 16:10:37 -0700

    Override onCreate() method in RestaurantListFragment.

--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListFragment.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListFragment.java
@@ -23,6 +23,10 @@ public class RestaurantListFragment extends BaseFragment {
     private RestaurantListAdapter mAdapter;
     public ArrayList<Restaurant> mRestaurants = new ArrayList<>();
 
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
 }
