efd12b64ad52ecbc412d968b1056c894638d6c11
courtney <courtney.mae.phillips@gmail.com>
2016-05-09 12:50:33 -0700

    Change instance of 'this' to 'getActivity()' to reflect method's new location in fragment, which does not have own context. Must instead retrieve context from corresponding activity.

--- app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListFragment.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListFragment.java
@@ -3,6 +3,7 @@ package com.epicodus.myrestaurants.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
@@ -12,7 +13,9 @@ import android.view.ViewGroup;
 import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
 import com.epicodus.myrestaurants.adapters.FirebaseRestaurantListAdapter;
+import com.epicodus.myrestaurants.models.Restaurant;
 import com.epicodus.myrestaurants.util.OnStartDragListener;
+import com.epicodus.myrestaurants.util.SimpleItemTouchHelperCallback;
 import com.firebase.client.Firebase;
 import com.firebase.client.Query;
 
@@ -43,4 +46,38 @@ public class SavedRestaurantListFragment extends BaseFragment implements OnStart
         return view;
     }
 
+    private void setUpFirebaseQuery() {
+        String userUid = mSharedPreferences.getString(Constants.KEY_UID, null);
+        String location = mFirebaseRestaurantsRef.child(userUid).toString();
+        mQuery = new Firebase(location).orderByChild("index");
+    }
+
+    private void setUpRecyclerView() {
+        mAdapter = new FirebaseRestaurantListAdapter(mQuery, Restaurant.class, this);
+        //In line below, we change 'this' to 'getActivity()' because fragments do not have own context:
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(mAdapter);
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        String uid = mSharedPreferences.getString(Constants.KEY_UID, null);
+        for (Restaurant restaurant : mAdapter.getItems()) {
+            String pushID = restaurant.getPushId();
+            restaurant.setIndex(Integer.toString(mAdapter.getItems().indexOf(restaurant)));
+            mFirebaseRestaurantsRef.child(uid)
+                    .child(pushID)
+                    .setValue(restaurant);
+        }
+    }
 }
