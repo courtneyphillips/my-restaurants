f405f954657dbef45a16024e78d958a93a54ed7d
courtney <courtney.mae.phillips@gmail.com>
2016-05-09 16:49:23 -0700

    Add onCreate() and onCreateView() methods to SavedRestaurantListFragment.

--- app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListFragment.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
 import com.epicodus.myrestaurants.adapters.FirebaseRestaurantListAdapter;
 import com.epicodus.myrestaurants.util.OnStartDragListener;
@@ -16,6 +17,7 @@ import com.firebase.client.Firebase;
 import com.firebase.client.Query;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SavedRestaurantListFragment extends BaseFragment implements OnStartDragListener {
     private Query mQuery;
@@ -23,5 +25,20 @@ public class SavedRestaurantListFragment extends BaseFragment implements OnStart
     private FirebaseRestaurantListAdapter mAdapter;
     private ItemTouchHelper mItemTouchHelper;
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mFirebaseRestaurantsRef = new Firebase(Constants.FIREBASE_URL_RESTAURANTS);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_saved_restaurant_list, container, false);
+        ButterKnife.bind(this, view);
+        setUpFirebaseQuery();
+        setUpRecyclerView();
+        return view;
+    }
 
 }
