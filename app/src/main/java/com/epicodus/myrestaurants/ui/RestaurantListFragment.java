fa8f3852bdcd8c9a1f079818c0d218197dcf8004
courtney <courtney.mae.phillips@gmail.com>
2016-05-09 16:06:08 -0700

    Extend BaseFragment in RestaurantListFragment.

--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListFragment.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListFragment.java
@@ -9,22 +9,8 @@ import android.view.ViewGroup;
 
 import com.epicodus.myrestaurants.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class RestaurantListFragment extends Fragment {
+public class RestaurantListFragment extends BaseFragment {
 
 
-    public RestaurantListFragment() {
-        // Required empty public constructor
-    }
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurant_list, container, false);
-    }
 
 }
