98148ee1b69a413a85c97a53aa6a7297873fd750
courtney <courtney.mae.phillips@gmail.com>
2016-05-09 12:28:21 -0700

    Create SavedRestaurantListFragment and corresponding layout file.

---
+++ app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListFragment.java
@@ -0,0 +1,15 @@
+package com.epicodus.myrestaurants.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.epicodus.myrestaurants.R;
+
+public class SavedRestaurantListFragment extends Fragment {
+
+
+}
