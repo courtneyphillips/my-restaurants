f6a7eb58d46a6d20e17762aee81a96c3c37f6315
courtney <courtney.mae.phillips@gmail.com>
2016-05-09 12:29:21 -0700

    Extend BaseFragment and implement OnDragStartListener interface in new SavedRestaurantListFragment.

--- app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListFragment.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListFragment.java
@@ -8,8 +8,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.epicodus.myrestaurants.R;
+import com.epicodus.myrestaurants.util.OnStartDragListener;
 
-public class SavedRestaurantListFragment extends Fragment {
+public class SavedRestaurantListFragment extends BaseFragment implements OnStartDragListener {
 
 
 }
