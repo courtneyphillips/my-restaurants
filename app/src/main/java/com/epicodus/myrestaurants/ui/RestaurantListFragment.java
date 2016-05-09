d77943012a86b4f26c7f420652ec333cde8a8022
courtney <courtney.mae.phillips@gmail.com>
2016-05-09 16:07:47 -0700

    Declare member variables in RestaurantListFragment.

--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListFragment.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantListFragment.java
@@ -1,15 +1,27 @@
 package com.epicodus.myrestaurants.ui;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.epicodus.myrestaurants.R;
+import com.epicodus.myrestaurants.adapters.RestaurantListAdapter;
+import com.epicodus.myrestaurants.models.Restaurant;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
 
 public class RestaurantListFragment extends BaseFragment {
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    private String mRecentAddress;
+    private RestaurantListAdapter mAdapter;
+    public ArrayList<Restaurant> mRestaurants = new ArrayList<>();
 
 
 
