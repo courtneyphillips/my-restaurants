7cea81427ff35f78fabc53864a13108330fe5468
courtney <courtney.mae.phillips@gmail.com>
2016-05-09 15:49:10 -0700

    Populate BaseFragment with commonly-used code to logout, and access shared preferences.

--- app/src/main/java/com/epicodus/myrestaurants/ui/BaseFragment.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/BaseFragment.java
@@ -1,16 +1,35 @@
 package com.epicodus.myrestaurants.ui;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.epicodus.myrestaurants.R;
+import com.epicodus.myrestaurants.Constants;
+import com.firebase.client.Firebase;
+
 
 public class BaseFragment extends Fragment {
+    public SharedPreferences mSharedPreferences;
+    public SharedPreferences.Editor mSharedPreferencesEditor;
+    public Firebase mFirebaseRef;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mSharedPreferencesEditor = mSharedPreferences.edit();
+        mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
+    }
+
+    public void addToSharedPreferences(String location) {
+        mSharedPreferencesEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
+    }
+
+    public void logout() {
+        mFirebaseRef.unauth();
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
+    }
 }
